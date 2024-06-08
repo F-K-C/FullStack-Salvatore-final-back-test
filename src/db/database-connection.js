1e615eba37155043893decf0462df0e194f083b2
F-K-C <fakocarvalho@gmail.com>
2024-06-08 21:02:17 +0000

    Banco resolvido MongoDB

--- src/db/database-connection.js
+++ src/db/database-connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb')
 
 const dbUrl = process.env.DATABASE_URL
-const dbName = 'mongodb-integrar-com-frontend'
+const dbName = 'mongodb-final-back-test'
 
 const client = new MongoClient(dbUrl)
 
